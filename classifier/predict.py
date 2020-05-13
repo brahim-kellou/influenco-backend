af76aa97b4846525696bc24a2a348645a13c9a11
brahim-kellou <fb_kellou@esi.dz>
2020-05-13 12:43:18 +0100

    initialize classifier

---
+++ classifier/predict.py
@@ -0,0 +1,10 @@
+
+def load_model(checkpoint):
+    model = 0
+    return model
+
+
+def classify_influencer(profile, checkpoint):
+    is_influencer = 0
+    profile_insights = 0
+    return is_influencer, profile_insights
---
+++ classifier/train.py
@@ -0,0 +1,13 @@
+
+
+def build_model():
+    model = 0
+    return model
+
+
+def train(model, loaders):
+    return model
+
+
+def save_model(model):
+    return 0
